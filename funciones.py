6eca6ee199ec0a9ac8dd090c18b840634804d63d
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-25 12:35:08 +0100

    clase excepciones

--- funciones.py
+++ funciones.py
@@ -1,9 +1,18 @@
 class Excepciones:
-    pass
+    def __init__(self, usuario):
+        self.usuario = usuario
+    
+    def intento(self):
+        try: 
+            self.usuario == "vicente@eni.es"
+            print("¡Bienvenido Vicente!")
+        except:
+            print("Uauario incorrecto, introduce tu dirección de correo")
 
 print("Hola Vicente, por favor introduce tu correo electrónico para acceder")
 
 usuario = " "
-usuario = "t"
-usuario = "t@t.es"
-usuario = "vicente@eni.es"
+e = Excepciones(usuario)
+#usuario = "t"
+#usuario = "t@t.es"
+#usuario = "vicente@eni.es"
