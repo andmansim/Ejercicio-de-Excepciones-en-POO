942da355627ee58a9518e92c09a9093d47fea465
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-25 12:30:25 +0100

    pregunta inicial y usuario

--- funciones.py
+++ funciones.py
@@ -1,8 +1,9 @@
-from ssl import CERT_OPTIONAL
-
-
 class Excepciones:
     pass
 
-print("Introduce tu correo electrónico")
-usuario = input()
+print("Hola Vicente, por favor introduce tu correo electrónico para acceder")
+
+usuario = " "
+usuario = "t"
+usuario = "t@t.es"
+usuario = "vicente@eni.es"
