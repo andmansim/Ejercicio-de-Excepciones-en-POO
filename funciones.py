482d9ddba6764b08b3463fb859286cf8bb19ef29
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-31 19:03:56 +0200

    borro espacios, recoloco estructura y añado recursividad

--- funciones.py
+++ funciones.py
@@ -12,27 +12,28 @@ def intento(contador1):
     validacion = re.search("@", usuario) #si no está nos devuelve None
     print(contador1)
     print(validacion)
-
+    if contador1 == 1 and validacion == None:
+        AtaqueBloq
+        print("Por motivos de seguridad, su usuario se ha bloqueado")
+        
     if validacion != None and contador1 > 0:
         try:
             usuario == "vicente@eni.es"
-            print("¡Bienvenido Vicente!")
-            
-            
+            print("¡Bienvenido Vicente!") 
         except DiFormato:
             contador1 = contador1 - 1
             print(contador1)
             print("La dirección de correo tiene que tener la forma de xxx@xx.xx")
-
+            intento(contador1)
+            
     if validacion == None and contador1 > 1:
         contador1 = contador1 - 1
         print(contador1)
         UsIncorrecto
         print("Usuario incorrecto, introduce tu dirección de correo")
-            
-    if contador1 == 1 and validacion == None:
-        AtaqueBloq
-        print("Por motivos de seguridad, su usuario se ha bloqueado")
+        intento(contador1)
+      
+    
             
 
 contador = 3
