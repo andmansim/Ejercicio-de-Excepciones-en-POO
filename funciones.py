f85dc129d0cb39e2e97c1245bacc92a983f76732
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-31 19:01:21 +0200

    intento excepciones 3, y clase AtaqueBloq

--- funciones.py
+++ funciones.py
@@ -4,35 +4,35 @@ class UsIncorrecto(BaseException):
     pass
 class DiFormato(BaseException):
     pass
-    
+class AtaqueBloq(BaseException):
+    pass
+
 def intento(contador1):
     usuario = input()
     validacion = re.search("@", usuario) #si no está nos devuelve None
     print(contador1)
     print(validacion)
-    if contador1 == 1 and validacion == None:
-        print("Por motivos de seguridad, su usuario se ha bloqueado")
-    
-    while contador1 > 0: 
-        contador1 = contador1 - 1  
-        if validacion != None and contador1 > 0:
-            try:
-                usuario == "vicente@eni.es"
-                print("¡Bienvenido Vicente!")
-                break
-                
-            except DiFormato:
-                
-                print("La dirección de correo tiene que tener la forma de xxx@xx.xx")
-                contador1 = contador1 - 1
-                
-        if validacion == None and contador1 > 1:
 
-            raise UsIncorrecto 
-            print("Usuario incorrecto, introduce tu dirección de correo")
-            contador1 = contador1 - 1
+    if validacion != None and contador1 > 0:
+        try:
+            usuario == "vicente@eni.es"
+            print("¡Bienvenido Vicente!")
             
+            
+        except DiFormato:
+            contador1 = contador1 - 1
             print(contador1)
+            print("La dirección de correo tiene que tener la forma de xxx@xx.xx")
+
+    if validacion == None and contador1 > 1:
+        contador1 = contador1 - 1
+        print(contador1)
+        UsIncorrecto
+        print("Usuario incorrecto, introduce tu dirección de correo")
+            
+    if contador1 == 1 and validacion == None:
+        AtaqueBloq
+        print("Por motivos de seguridad, su usuario se ha bloqueado")
             
 
 contador = 3
