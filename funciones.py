d2b196a23ca87cf4fecd778326aa3de872fc74bc
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-31 20:00:24 +0200

    main terminado

--- funciones.py
+++ funciones.py
@@ -37,31 +37,5 @@ def intento(contador1):
         except AtaqueBloq:
             print("Por motivos de seguridad, su usuario se ha bloqueado")
             
-'''  if contador1 == 1 and validacion == None:
-        AtaqueBloq
-        print("Por motivos de seguridad, su usuario se ha bloqueado")
-        
-    if validacion != None and contador1 > 0:
-        try:
-            usuario == "vicente@eni.es"
-            print("¡Bienvenido Vicente!") 
-        except DiFormato:
-            contador1 = contador1 - 1
-            print(contador1)
-            print("La dirección de correo tiene que tener la forma de xxx@xx.xx")
-            intento(contador1)
-            
-    if validacion == None and contador1 > 1:
-        contador1 = contador1 - 1
-        print(contador1)
-        UsIncorrecto
-        print("Usuario incorrecto, introduce tu dirección de correo")
-        intento(contador1)
-      
-    
-            
-'''
-contador = 3
-print("Hola Vicente, por favor introduce tu correo electrónico para acceder")
-intento(contador)
+
 
---
+++ main.py
@@ -0,0 +1,6 @@
+import funciones
+
+if __name__ == "__main__":
+    contador = 3
+    print("Hola Vicente, por favor introduce tu correo electrónico para acceder")
+    funciones.intento(contador)
