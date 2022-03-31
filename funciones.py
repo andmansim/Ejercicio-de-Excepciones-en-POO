87eb7cca317ca955404f43f8cda8575cef8c1392
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-31 08:55:19 +0200

    mejoro la función intento

--- funciones.py
+++ funciones.py
@@ -13,21 +13,23 @@ import re
 def intento(contador):
     usuario = input()
     validacion = re.search("@", usuario) #si no está nos devuelve None
-    print(validacion)
+    #print(validacion)
     
-    while contador < 4:
-        if validacion != None:
-            if usuario == "vicente@eni.es":
-                print("¡Bienvenido Vicente!")
-            else:
-                print("La dirección de correo tiene que tener la forma de xxx@xx.xx")
-                contador = contador + 1
-                intento()
+    
+    if validacion != None:
+        if usuario == "vicente@eni.es":
+            print("¡Bienvenido Vicente!")
+        
         else:
-            print("Uauario incorrecto, introduce tu dirección de correo")
+            print("La dirección de correo tiene que tener la forma de xxx@xx.xx")
             contador = contador + 1
-            intento()
-    print("Por motivos de seguridad se le ha bloqueado")
+            intento(contador)
+    else:
+        print("Uauario incorrecto, introduce tu dirección de correo")
+        contador = contador + 1
+        intento(contador)
+    if contador > 4:
+        print("Por motivos de seguridad se le ha bloqueado")
 
 contador = 0
 print("Hola Vicente, por favor introduce tu correo electrónico para acceder")
