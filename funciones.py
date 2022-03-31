5b5fc642b362bb0bb985b5d6b3cef6cd156e13ec
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-31 18:09:51 +0200

    intento que funcione la clase UsIncorrecto

--- funciones.py
+++ funciones.py
@@ -1,14 +1,15 @@
 import re
 
 class UsIncorrecto(BaseException):
-    def __init__(self):
+    def __init__(self, contador):
         
         print("Usuario incorrecto, introduce tu dirección de correo")
-        self.contador = contador + 1
+        self.contador = contador - 1
+        print(self.contador)
         intento(self.contador)
         
-''' def get_usuario(self): #método get, nos lo coge
-        return self.usuario
+''' def get_contador(self): #método get, nos lo coge
+        return self.contador
     
     def set_usuario(self, usuario): #método set, nos servirá para modificar el sself.usuario
         self.usuario = usuario'''
@@ -18,25 +19,25 @@ def intento(contador):
     validacion = re.search("@", usuario) #si no está nos devuelve None
     print(contador)
     
-    if contador >= 3 and validacion == None:
+    if contador == 1 and validacion == None:
         print("Por motivos de seguridad se le ha bloqueado")
         
-    if contador <= 3:
-        if validacion != None:
-            if usuario == "vicente@eni.es":
-                print("¡Bienvenido Vicente!")
-                
-            else:
-                print("La dirección de correo tiene que tener la forma de xxx@xx.xx")
-                contador = contador + 1
-                intento(contador)
-        elif validacion == None:
-            raise UsIncorrecto
-'''            print("Usuario incorrecto, introduce tu dirección de correo")
-            contador = contador + 1
-            intento(contador)'''
+    
+    if validacion != None and contador > 0:
+        if usuario == "vicente@eni.es":
+            print("¡Bienvenido Vicente!")
             
-contador = 1
+        else:
+            print("La dirección de correo tiene que tener la forma de xxx@xx.xx")
+            contador = contador - 1
+            intento(contador)
+    elif validacion == None and contador > 1:
+        raise UsIncorrecto(contador)
+'''            print("Usuario incorrecto, introduce tu dirección de correo")
+        contador = contador + 1
+        intento(contador)'''
+            
+contador = 3
 print("Hola Vicente, por favor introduce tu correo electrónico para acceder")
 intento(contador)
 #e = Excepciones(usuario)
